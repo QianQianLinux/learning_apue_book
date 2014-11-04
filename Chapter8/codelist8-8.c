61076cfb434e70cef3ff6b3f9bd85cc67f6fb5cb
Donyzh <Donyzh@example.com>
2014-11-04 18:37:31 +0800

    add codelist8-8 8-9

--- Chapter8/codelist8-8.c
+++ Chapter8/codelist8-8.c
@@ -1,4 +1,6 @@
 //程序清单8-8 exec函数实例
+//要正确运行程序需将echoall拷贝到某PATH环境变量目录下
+//原因见http://blog.csdn.net/abcdef0966/article/details/5725541
 
 #include "apue.h"
 #include <sys/wait.h>
@@ -8,5 +10,24 @@ char	*env_init[] = { "USER=unknown", "PATH=/tmp", NULL };
 int main(void)
 {
 	pid_t	pid;
+	if ((pid = fork()) < 0) {
+		err_sys("fork error");
+	} else if (pid == 0) { /* specify pathname, specify environment */
+	   if (execle("./echoall",
+		"echoall", "myarg1", "MY ARG2", (char *)0, env_init) < 0)
+	      err_sys("execle error");
+	}
+
+	if (waitpid(pid, NULL, 0) < 0)
+		err_sys("wait error");
+	
+	if ((pid = fork()) < 0) {
+		err_sys("fork error");
+	} else if (pid == 0) { /* specify filename, inherit environment */
+	    if (execlp("echoall", "echoall", "only 1 arg", (char *)0) < 0)
+		err_sys("execlp error");
+	}
+
+	exit(0);
 
 }
